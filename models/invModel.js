6f820520c88dc456b2c502b03b779e73a469ae52
Ibrahim Shehadeh <134406620+TheHeisenBurglar@users.noreply.github.com>
2024-04-27 14:56:43 -0400

    Update invModel.js
    added cost field,
    made voh required status false.

--- models/invModel.js
+++ models/invModel.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose")
 const invSchema = mongoose.Schema({
     name:{type:String, required:true},
     company:{type:String, required:true},
-    voh: {type:String, required:true},
+    voh: {type:String, required:false},
     dim: {type:String, required:true},
     tag: {type:String, required:true},
+    cost: {type:String, required:false},
 },{
     versionKey:false
 });
